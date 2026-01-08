bb8fbce21d352acc381eb98b3415a5acdc5a9280
yoni182 <yoni.chesla@techsee.me>
2026-01-08 17:23:56 +0200

    Simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  server: {
-    port: 3003,
-    host: '0.0.0.0',
-    proxy: {
-      // Proxy API calls to Vercel dev server during local development
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
-    }
-  },
   plugins: [react()],
   resolve: {
     alias: {
@@ -21,4 +10,3 @@ export default defineConfig({
     }
   }
 });
-
